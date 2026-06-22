d8c133be4b22ccd03b8e2a0bb87c2387e655e99b
shanAweb <shanasif2002@gmail.com>
2026-06-22 11:43:07 +0500

    chore: just checking a pattern while switching a path sequence

--- backend/drf_work/api/urls.py
+++ backend/drf_work/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-    path('health', views.get_status),
     path('data', views.endpoint_view),
+    path('health', views.get_status),
 ]
